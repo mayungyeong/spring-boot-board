f73229949b6459ca5c7979ff560b2769120f4180
mayungyeong <kwn07191@naver.com>
2023-03-26 21:03:10 +0900

    게시글 작성, 수정 메시지

--- src/main/java/com/simple/board/controller/BoardController.java
+++ src/main/java/com/simple/board/controller/BoardController.java
@@ -22,11 +22,14 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board) {
+    public String boardWritePro(Board board, Model model) {
 
         boardService.write(board);
 
-        return "";
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
     }
 
     @GetMapping("/board/list")
@@ -58,7 +61,7 @@ public class BoardController {
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board){
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model){
 
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
@@ -66,7 +69,10 @@ public class BoardController {
 
         boardService.write(boardTemp);
 
-        return "redirect:/board/list";
+        model.addAttribute("message", "글 수정이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
 
     }
 }
