e51d6012a5ae35514b3915c05db6b03ae29d2e8a
mayungyeong <kwn07191@naver.com>
2023-03-26 17:27:18 +0900

    Repository

---
+++ src/main/java/com/simple/board/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.simple.board.repository;
+
+import com.simple.board.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Integer> {
+}
